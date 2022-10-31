cf3bbf67254722616753da1d2895406cd32a598a
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-31 23:50:02 +0300

    Complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr.Chocolate",2,"M","Brown","sad",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",2,"M","Brown",NULL,1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha",2,"F","Brown",NULL,0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",2,"M","Brown",NULL,1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melisa",2,"F","Brown",NULL,0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch",2,"F","Brown",NULL,1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy",2,"F","Brown",NULL,1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,2,"M","Brown",NULL,1);
